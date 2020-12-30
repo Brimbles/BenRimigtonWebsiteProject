0fdea1c97eda13684d1aaa937c99fa8f3bc06fb7
Ben Rimington <benrimington@googlemail.com>
2020-12-30 09:41:57 +0000

    added static_root value to settings.py

--- benrimingtondotcom/settings.py
+++ benrimingtondotcom/settings.py
@@ -133,6 +133,9 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles') #The absolute path to the directory where collectstatic will collect static files for deployment.
+
+
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 
